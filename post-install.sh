c431bff87b75d8238477dd13417374ccf0e82be6
Guillaume Plourde <gplourde@protonmail.com>
2023-08-24 09:47:23 -0400

    add proton bridge (#5)
    Signed-off-by: Guillaume <gplourde@proton.me>
    Co-authored-by: Guillaume <gplourde@proton.me>

--- post-install.sh
+++ post-install.sh
@@ -15,5 +15,8 @@ ln -s /etc/alternatives/ld /usr/bin/ld
 
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-
 rpm-ostree install code
+
+# proton bridge 
+wget -P /tmp/rpms https://proton.me/download/bridge/protonmail-bridge-3.3.2-1.x86_64.rpm
+rpm-ostree install /tmp/rpms/protonmail-bridge*rpm
