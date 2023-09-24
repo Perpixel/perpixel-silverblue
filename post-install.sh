7d67401e67cd3b021521ebf13d7b7ce0bc68899a
Guillaume <gplourde@proton.me>
2023-09-24 16:21:19 -0400

    add terminals
    Signed-off-by: Guillaume <gplourde@proton.me>

--- post-install.sh
+++ post-install.sh
@@ -6,5 +6,6 @@ set -ouex pipefail
 ln -s /usr/bin/ld.bfd /etc/alternatives/ld
 ln -s /etc/alternatives/ld /usr/bin/ld
 
+# Proton mail
 wget -P /tmp/rpms https://proton.me/download/bridge/protonmail-bridge-3.3.2-1.x86_64.rpm
 rpm-ostree install /tmp/rpms/protonmail-bridge*rpm
--- pre-install.sh
+++ pre-install.sh
@@ -2,13 +2,19 @@
 
 set -ouex pipefail
 
-#sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
-
+# download and install rpm fusion package
 wget -P /tmp/rpms \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${FEDORA_MAJOR_VERSION}.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${FEDORA_MAJOR_VERSION}.noarch.rpm
 
-rpm-ostree install \
-    /tmp/rpms/*.rpm \
-    fedora-repos-archive
+rpm-ostree install /tmp/rpms/*.rpm fedora-repos-archive
 
+#sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
+
+# enable
+sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/fedora-cisco-openh264.repo
+
+# disable
+sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/fedora-updates-testing.repo
+sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/rpmfusion-nonfree-updates-testing.repo
+sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/rpmfusion-free-updates-testing.repo
