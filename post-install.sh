ac4fede4827499d029b894e4b70373fe17845ca8
Guillaume <gplourde@proton.me>
2023-09-30 17:21:29 -0400

    add rpm cache cleanup to save space
    Signed-off-by: Guillaume <gplourde@proton.me>

--- post-install.sh
+++ post-install.sh
@@ -9,3 +9,6 @@ ln -s /etc/alternatives/ld /usr/bin/ld
 # Proton mail
 wget -P /tmp/rpms https://proton.me/download/bridge/protonmail-bridge-3.3.2-1.x86_64.rpm
 rpm-ostree install /tmp/rpms/protonmail-bridge*rpm
+
+# cleanup
+rpm-ostree cleanup
