10b17b7a0acbe0907a9b520f2ba904bb4fe1a336
Guillaume Plourde <gplourde@protonmail.com>
2024-05-09 20:44:16 -0400

    Update build-nvidia-rpm.sh

--- build-nvidia-rpm.sh
+++ build-nvidia-rpm.sh
@@ -2,24 +2,27 @@
 
 set -oeux pipefail
 
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,updates-testing}.repo
+
 RELEASE="$(rpm -E '%fedora.%_arch')"
 
 NVIDIA_PACKAGE_NAME="nvidia"
 
 rpm-ostree install \
-	akmod-${NVIDIA_PACKAGE_NAME}*:${NVIDIA_MAJOR_VERSION}.*.fc${RELEASE} \
-	xorg-x11-drv-${NVIDIA_PACKAGE_NAME}-{,cuda,devel,kmodsrc,power}*:${NVIDIA_MAJOR_VERSION}.*.fc${RELEASE}
+    akmod-${NVIDIA_PACKAGE_NAME}*:${NVIDIA_MAJOR_VERSION}.*.fc${RELEASE} \
+    xorg-x11-drv-${NVIDIA_PACKAGE_NAME}-{,cuda,devel,kmodsrc,power}*:${NVIDIA_MAJOR_VERSION}.*.fc${RELEASE} \
+    mock
 
 # alternatives cannot create symlinks on its own during a container build
 ln -s /usr/bin/ld.bfd /etc/alternatives/ld && ln -s /etc/alternatives/ld /usr/bin/ld
 
 if [[ ! -s "/tmp/certs/private_key.priv" ]]; then
-	echo "WARNING: Using test signing key. Run './generate-akmods-key' for production builds."
-	cp /tmp/certs/private_key.priv{.test,}
-	cp /tmp/certs/public_key.der{.test,}
+    echo "WARNING: Using test signing key. Run './generate-akmods-key' for production builds."
+    cp /tmp/certs/private_key.priv{.test,}
+    cp /tmp/certs/public_key.der{.test,}
 fi
 
-install -Dm644 /tmp/certs/public_key.der /etc/pki/akmods/certs/public_key.der
+install -Dm644 /tmp/certs/public_key.der   /etc/pki/akmods/certs/public_key.der
 install -Dm644 /tmp/certs/private_key.priv /etc/pki/akmods/private/private_key.priv
 
 # Either successfully build and install the kernel modules, or fail early with debug output
@@ -30,10 +33,10 @@ NVIDIA_FULL_VERSION="$(rpm -q "xorg-x11-drv-${NVIDIA_PACKAGE_NAME}" --queryforma
 
 akmods --force --kernels "${KERNEL_VERSION}" --kmod "${NVIDIA_PACKAGE_NAME}"
 
-modinfo /usr/lib/modules/${KERNEL_VERSION}/extra/${NVIDIA_PACKAGE_NAME}/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.xz >/dev/null || \
-	(cat /var/cache/akmods/${NVIDIA_PACKAGE_NAME}/${NVIDIA_AKMOD_VERSION}-for-${KERNEL_VERSION}.failed.log && exit 1)
+modinfo /usr/lib/modules/${KERNEL_VERSION}/extra/${NVIDIA_PACKAGE_NAME}/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.xz > /dev/null || \
+(cat /var/cache/akmods/${NVIDIA_PACKAGE_NAME}/${NVIDIA_AKMOD_VERSION}-for-${KERNEL_VERSION}.failed.log && exit 1)
 
-cat <<EOF >/var/cache/akmods/nvidia-vars
+cat <<EOF > /var/cache/akmods/nvidia-vars
 KERNEL_VERSION=${KERNEL_VERSION}
 RELEASE=${RELEASE}
 NVIDIA_PACKAGE_NAME=${NVIDIA_PACKAGE_NAME}
