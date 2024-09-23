661549ab23a12fe715b7bc175d31c882426427c2
Guillaume <gplourde@proton.me>
2024-09-23 16:45:34 -0400

    support fc41

--- build_files/scripts/build-nvidia-drv.sh
+++ build_files/scripts/build-nvidia-drv.sh
@@ -10,12 +10,12 @@ mkdir -p /tmp/nvidia-drv
 cd /tmp/nvidia-drv
 
 # dnf update 4.21 beak akmods build
-rpm-ostree install yum-4.19.* python3-dnf-plugin-versionlock
-yum versionlock dnf-4.19
-yum versionlock dnf-4.19.*
-yum versionlock dnf-data-4.19.*
+#rpm-ostree install yum-4.19.* python3-dnf-plugin-versionlock
+#yum versionlock dnf-4.19
+#yum versionlock dnf-4.19.*
+#yum versionlock dnf-data-4.19.*
 
-wget https://github.com/Perpixel/nvidia-driver-rpms/releases/download/v${NVIDIA_VERSION}/nvidia-drv-${NVIDIA_VERSION}.tar.gz
+wget https://github.com/Perpixel/nvidia-driver-rpms/releases/download/v${NVIDIA_VERSION}/nvidia-drv-${NVIDIA_VERSION}.fc${RELEASE}.tar.gz
 tar -zxf nvidia-drv-*.tar.gz
 
 rpm-ostree install \
--- local_build.sh
+++ local_build.sh
@@ -3,7 +3,7 @@
 export REPO="perpixel-silverblue"
 export BASE_IMAGE="quay.io/fedora-ostree-desktops/silverblue"
 export NVIDIA_IMAGE_NAME="nvidia-${REPO}"
-export FEDORA_MAJOR_VERSION="40"
+export FEDORA_MAJOR_VERSION="41"
 export NVIDIA_VERSION="560.35.03"
 export VERSION_TAG="local-${FEDORA_MAJOR_VERSION}-${NVIDIA_VERSION}"
 export TIMESTAMP="$(date +%Y%m%d)"
