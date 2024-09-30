15654bdc92653271738630c8a2b1175da7c99fb8
Guillaume Plourde <gplourde@protonmail.com>
2024-09-30 13:49:55 -0400

    Update build-nvidia-drv.sh

--- build_files/scripts/build-nvidia-drv.sh
+++ build_files/scripts/build-nvidia-drv.sh
@@ -29,12 +29,18 @@ setup_rpm_build_env() {
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${FEDORA_MAJOR_VERSION}.noarch.rpm
   dnf install /tmp/rpms/*.rpm fedora-repos-archive -y
 
+  # dnf install \
+  # rpm-build rpmspectool libappstream-glib systemd-rpm-macros rpmdevtools gcc \
+  # mesa-libGL-devel mesa-libEGL-devel libvdpau-devel libXxf86vm-devel libXv-devel \
+  # desktop-file-utils hostname gtk3-devel m4 pkgconfig mock libtirpc-devel \
+  # buildsys-build-rpmfusion-kerneldevpkgs-current elfutils-libelf-devel vulkan-headers -y
+  # }
+
   dnf install \
     rpm-build rpmspectool libappstream-glib systemd-rpm-macros rpmdevtools gcc \
-    mesa-libGL-devel mesa-libEGL-devel libvdpau-devel libXxf86vm-devel libXv-devel \
-    desktop-file-utils hostname gtk3-devel m4 pkgconfig mock libtirpc-devel \
-    buildsys-build-rpmfusion-kerneldevpkgs-current elfutils-libelf-devel vulkan-headers -y
-}
+    desktop-file-utils hostname m4 pkgconfig mock libtirpc-devel \
+    buildsys-build-rpmfusion-kerneldevpkgs-current elfutils-libelf-devel -y
+  }
 
 setup_sources() {
   echo Setting up ${1} sources...
