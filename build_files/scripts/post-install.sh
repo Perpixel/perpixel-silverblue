e20270f267d82963ce1e7bc7a48549df87453a8b
Guillaume Plourde <gplourde@protonmail.com>
2024-10-03 21:59:24 -0400

    Update post-install.sh

--- build_files/scripts/post-install.sh
+++ build_files/scripts/post-install.sh
@@ -7,5 +7,5 @@ set -ouex pipefail
 # rpm-ostree install /tmp/rpms/ProtonMail-*.rpm
 
 # nvidia-container-toolkit
-# curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
-# rpm-ostree install -y nvidia-container-toolkit
+curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+rpm-ostree install -y nvidia-container-toolkit
