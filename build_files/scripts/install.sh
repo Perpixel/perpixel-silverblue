481dda2d2ceed6b76dd38e7fa19ebea0cbb7953a
Guillaume Plourde <gplourde@protonmail.com>
2024-10-09 13:01:57 -0400

    Update install.sh

--- build_files/scripts/install.sh
+++ build_files/scripts/install.sh
@@ -27,7 +27,7 @@ sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/rpmfusion-nonfree-updates.repo
 sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/rpmfusion-free-updates.repo
 
 # disable
-sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/fedora-cisco-openh264.repo
+# sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/fedora-cisco-openh264.repo
 sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/fedora-updates.repo
 sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/fedora-updates-testing.repo
 sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/fedora-updates-archive.repo
