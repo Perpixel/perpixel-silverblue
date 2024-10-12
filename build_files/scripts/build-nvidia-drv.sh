2ad5efec4a7bf6d0a25b6fd06ddd040b85215cb7
Guillaume Plourde <gplourde@protonmail.com>
2024-10-12 13:41:14 -0400

    update

--- build_files/scripts/build-nvidia-drv.sh
+++ build_files/scripts/build-nvidia-drv.sh
@@ -4,8 +4,6 @@ set -oeux pipefail
 
 source "$(dirname "$0")"/functions.sh
 
-dnf config-manager --set-disabled fedora-cisco-openh264
-
 disable-repo /etc/yum.repos.d/fedora-cisco-openh264.repo
 disable-repo /etc/yum.repos.d/fedora-updates-testing.repo
 disable-repo /etc/yum.repos.d/fedora-updates-archive.repo
