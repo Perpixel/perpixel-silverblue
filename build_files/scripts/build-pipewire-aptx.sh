4db5f2cf7ec6c35f488bd433eeaca9a16f5773c0
Guillaume <gplourde@proton.me>
2024-10-25 18:05:41 -0400

    add pipewire aptx

--- build_files/scripts/build-pipewire-aptx.sh
+++ build_files/scripts/build-pipewire-aptx.sh
@@ -4,12 +4,15 @@ SPA_VERSION=0.2
 PIPEWIRE_VERSION=$(rpm -q --queryformat '%{VERSION}' pipewire)
 BUILT_DIR=/tmp/built
 
+mkdir /tmp/pipewire
+pushd /tmp/pipewire
+
 # install build deps
 dnf install -y meson gcc cmake glib2-devel dbus-devel sbc-devel bluez-libs-devel
 
 # install libfreeaptx
-git clone https://github.com/iamthehorker/libfreeaptx /tmp/build/libfreeaptx
-pushd /tmp/build/libfreeaptx
+git clone https://github.com/iamthehorker/libfreeaptx /tmp/pipewire/libfreeaptx
+pushd /tmp/pipewire/libfreeaptx
 make
 make install PREFIX= LIBDIR="/usr/lib64" INCDIR="/usr/include/" BINDIR="/usr/bin"
 make install PREFIX= LIBDIR="${BUILT_DIR}/usr/lib64" INCDIR="${BUILT_DIR}/usr/include/" BINDIR="${BUILT_DIR}/usr/bin"
@@ -17,17 +20,17 @@ ldconfig
 popd
 
 # build bluez5-codec-aptx
-rm -rf /tmp/build/pipewire
+rm -rf /tmp/pipewire/src
 # get source
-git clone --depth 1 --branch "${PIPEWIRE_VERSION}" https://gitlab.freedesktop.org/pipewire/pipewire.git /tmp/build/pipewire
-pushd /tmp/build/pipewire
+git clone --depth 1 --branch "${PIPEWIRE_VERSION}" https://gitlab.freedesktop.org/pipewire/pipewire.git /tmp/pipewire/src
+pushd /tmp/pipewire/src
 meson setup build
 meson configure build -D bluez5-codec-aptx=enabled --auto-features=disabled \
   -D examples=disabled -D bluez5=enabled -D bluez5-codec-aptx=enabled \
   -D session-managers=[]
 meson compile -C build
-mkdir -p /tmp/build/usr/lib64/spa-"${SPA_VERSION}"/bluez5
+mkdir -p "${BUILT_DIR}"/usr/lib64/spa-"${SPA_VERSION}"/bluez5
 install -pm 0755 ./build/spa/plugins/bluez5/libspa-codec-bluez5-aptx.so \
-  /tmp/build/usr/lib64/spa-"${SPA_VERSION}"/bluez5/
+  "${BUILT_DIR}"/usr/lib64/spa-"${SPA_VERSION}"/bluez5/
 ldconfig
 popd
