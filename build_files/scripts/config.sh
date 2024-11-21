ba670d2cd11f744eb70d79f64e52b23f44d83a2f
Guillaume <gplourde@proton.me>
2024-11-21 16:27:59 -0500

    update

--- build_files/scripts/config.sh
+++ build_files/scripts/config.sh
@@ -1,4 +1,4 @@
-TARGET_IMAGE_NAME="perpixel-silverblue"
-BASE_IMAGE="quay.io/fedora-ostree-desktops/silverblue"
-FEDORA_VERSION="41"
-NVIDIA_VERSION="565.57.01"
+TARGET_IMAGE_NAME=perpixel-silverblue
+BASE_IMAGE=quay.io/fedora-ostree-desktops/silverblue
+FEDORA_VERSION=41
+NVIDIA_VERSION=565.57.01
