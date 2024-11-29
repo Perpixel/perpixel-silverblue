0033335162a864a6cd1b4d6ac15b0fce8852f91f
Guillaume <gplourde@proton.me>
2024-11-28 21:21:07 -0500

    add script to verify nvidia drivers and settings

--- nvidia-check.sh
+++ nvidia-check.sh
@@ -1,5 +1,37 @@
-# is drm modeset enabled
-cat /sys/module/nvidia_drm/parameters/modeset
+#!/bin/bash
 
-# is fbdev enabled
-cat /sys/module/nvidia_drm/parameters/fbdev
+echo "### Kernel command line"
+cat /proc/cmdline
+echo
+
+echo "### Nvidia driver version"
+cat /proc/driver/nvidia/version
+echo
+
+echo "### loaded nvidia modules"
+lsmod | grep nvidia
+echo
+
+echo "### Modules parameters"
+echo Module: nvidia
+cat /proc/driver/nvidia/params
+echo
+
+declare -a modules=("nvidia_drm" "nvidia_modset" "nvidia_uvm")
+for module in "${modules[@]}"; do
+  echo "$module:"
+  if [ -d "/sys/module/$module/parameters" ]; then
+    ls /sys/module/$module/parameters/ | while read param; do
+      echo -n "    $param: "
+      cat /sys/module/$module/parameters/$param
+    done
+  fi
+  echo
+done
+
+echo "### Nvidia services status"
+declare -a services=("nvidia-hibernate" "nvidia-resume" "nvidia-suspend" "nvidia-persistenced" "nvidia-powerd")
+echo Services:
+for service in "${services[@]}"; do
+  echo "    $service: $(systemctl is-enabled ${service})"
+done
