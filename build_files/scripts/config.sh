eb055e38e02b75964091142684e738af42b191ea
Guillaume <gplourde@proton.me>
2024-12-10 19:31:13 -0500

    nvidia 565.77

--- build_files/scripts/config.sh
+++ build_files/scripts/config.sh
@@ -2,6 +2,4 @@ TARGET_IMAGE_NAME=perpixel-silverblue
 BASE_IMAGE=quay.io/fedora-ostree-desktops/silverblue
 FEDORA_VERSION=41
 USE_LTS_KERNEL=true
-NVIDIA_VERSION=565.57.01 # beta
-# NVIDIA_VERSION=560.35.03 # new feature branch
-# NVIDIA_VERSION=550.135 # stable
+NVIDIA_VERSION=565.77
