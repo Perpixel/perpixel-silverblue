7d2a000d16d8ce08a52d7c5fb829e39388127661
Guillaume Plourde <gplourde@proton.me>
2025-08-21 22:35:35 -0400

    roll back to nvidia driver 575

--- build_files/scripts/config.sh
+++ build_files/scripts/config.sh
@@ -2,4 +2,4 @@ TARGET_IMAGE_NAME=perpixel-silverblue
 BASE_IMAGE=quay.io/fedora-ostree-desktops/silverblue
 FEDORA_VERSION=42
 USE_LTS_KERNEL=false
-NVIDIA_VERSION=580.76.05
+NVIDIA_VERSION=575.64.05
